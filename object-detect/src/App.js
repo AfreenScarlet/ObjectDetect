3237f3fbeec6dcafe16c39659e567f07745a42c1
AfreenScarlet <titania.afreen@gmail.com>
2019-09-22 21:57:08 +0530

    object detection

---
+++ object-detect/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+import ImageOps from './containers/ImageOps';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageOps />
+    </div>
+  );
+}
+
+
+export default App;
